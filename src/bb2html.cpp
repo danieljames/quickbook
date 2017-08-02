e7b49cecf8d65f52134bf84fb93d6b9dda6312df
Daniel James <daniel@calamity.org.uk>
2017-08-02 23:04:54 +0100

    Lower level tag functions

--- src/bb2html.cpp
+++ src/bb2html.cpp
@@ -611,10 +611,29 @@ namespace quickbook { namespace detail {
     typedef boost::unordered_map<quickbook::string_view, node_parser> node_parsers_type;
     static node_parsers_type node_parsers;
 
-    void open_tag(html_gen& gen, quickbook::string_view name) {
+    void tag_start(html_gen& gen, quickbook::string_view name) {
         gen.html += "<";
         gen.html.append(name.begin(), name.end());
+    }
+
+    void tag_attribute(html_gen& gen, quickbook::string_view name, quickbook::string_view value) {
+        gen.html += " ";
+        gen.html.append(name.begin(), name.end());
+        gen.html += "=\"";
+        gen.html += "\"";
+    }
+
+    void tag_end(html_gen& gen) {
         gen.html += ">";
+    }
+
+    void tag_end_close(html_gen& gen) {
+        gen.html += "/>";
+    }
+
+    void open_tag(html_gen& gen, quickbook::string_view name) {
+        tag_start(gen, name);
+        tag_end(gen);
     }
 
     void close_tag(html_gen& gen, quickbook::string_view name) {
@@ -623,9 +642,9 @@ namespace quickbook { namespace detail {
         gen.html += ">";
     }
 
-    void tag(html_gen& gen, quickbook::string_view name, xml_element* children) {
+    void tag(html_gen& gen, quickbook::string_view name, xml_element* x) {
         open_tag(gen, name);
-        document(gen, children);
+        document(gen, x->children());
         close_tag(gen, name);
     }
 
@@ -664,7 +683,7 @@ namespace quickbook { namespace detail {
 
 #define NODE_MAP(tag_name, html_name) \
     NODE_RULE(tag_name, gen, x) { \
-        tag(gen, BOOST_PP_STRINGIZE(html_name), x->children()); \
+        tag(gen, BOOST_PP_STRINGIZE(html_name), x); \
     }
 
     NODE_MAP(para, p)
@@ -750,7 +769,7 @@ namespace quickbook { namespace detail {
             tag_name = "strong";
         }
         // TODO: Error on unrecognized role + case insensitive
-        return tag(gen, tag_name, x->children());
+        return tag(gen, tag_name, x);
     }
 
     NODE_RULE(inlinemediaobject, gen, x) {
