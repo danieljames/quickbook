458cf2e2e239ccefd31752fad01ab1a594c73bbd
Daniel James <daniel@calamity.org.uk>
2017-08-04 11:52:12 +0100

    No need to pass path to inline_chunks

--- src/bb2html.cpp
+++ src/bb2html.cpp
@@ -418,11 +418,11 @@ namespace quickbook { namespace detail {
     void generate_chunked_documentation(chunk* root, id_paths_type const&, fs::path const& root_path);
     void generate_file_documentation(chunk* root, id_paths_type const&, fs::path const& path);
 
-    void inline_chunks(chunk* c, string_view path) {
+    void inline_chunks(chunk* c) {
         for(;c; c = c->next()) {
             c->inline_ = true;
-            c->path_.assign(path.begin(), path.end());
-            inline_chunks(c->children(), path);
+            c->path_ = c->parent()->path_;
+            inline_chunks(c->children());
         }
     }
 
@@ -430,7 +430,7 @@ namespace quickbook { namespace detail {
         // When depth is 0, inline leading sections.
         if (depth == 0) {
             for (;c && c->root_->name_ == "section"; c = c->next()) {
-                inline_chunks(c, c->parent()->path_);
+                inline_chunks(c);
             }
         }
 
@@ -491,7 +491,7 @@ namespace quickbook { namespace detail {
         } else {
             std::string path = path_to_generic(output_path.filename());
             chunked->path_ = path;
-            inline_chunks(chunked->children(), path);
+            inline_chunks(chunked->children());
         }
         id_paths_type id_paths = get_id_paths(chunked);
         if (chunked_output) {
